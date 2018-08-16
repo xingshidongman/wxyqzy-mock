200df9735a64814135613e79498f3d2b35880721
juezhan <11991762@qq.com>
2018-08-16 16:30:57 +0800

    增加 department

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const bodyParts = require('./bodyParts')
 const illness = require('./illness')
+const department = require('./department')
 
 module.exports = function () {
   return {
     body_parts: bodyParts(),
-    illness: illness()
+    illness: illness(),
+    department: department()
   }
 }
